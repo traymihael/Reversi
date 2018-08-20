66848d20c57614aee19643b245c873eea2997e2b
traymihael <kazuki19950902@yahoo.co.jp>
2018-08-20 16:47:31 +0900

    0820

--- reversi.py
+++ reversi.py
@@ -1,17 +1,161 @@
 def get_initial_place():
-    initial_place = [['―' for i in range(8)] for j in range(8)]
-    initial_place[3][3] = '●'
-    initial_place[4][4] = '●'
-    initial_place[3][4] = '○'
-    initial_place[4][3] = '○'
+    initial_place = [['-' for i in range(8)] for j in range(8)]
+    initial_place[3][3] = 'w'
+    initial_place[4][4] = 'w'
+    initial_place[3][4] = 'b'
+    initial_place[4][3] = 'b'
     return initial_place
 
-def put_koma(locate_list, point_inf):
-    y, x, color = point_inf.split()
-    y = ord(y) - 97
-    if color == 'B':
-        color = '●'
-    else:
-        color = '○'
-    locate_list[int(x)][int(y)] = color
+def put_koma(locate_list, x, y, turn):
+    locate_list[int(x)][int(y)] = turn
     return locate_list
+
+
+def get_position():
+    position_data = []
+    for i in range(8):
+        position_data.append([])
+        for j in range(8):
+            position_data[-1].append('{}_{}'.format(i, j))
+    return position_data
+
+def change_bord(locate_list, location, turn):
+    everywhere = [[1, 1],[1, 0],[0, 1],[-1, 1],[-1, 0],[1, -1],[0, -1],[-1, -1]]
+    location = list(map(int, location.split('_')))
+
+    locate_list = put_koma(locate_list, location[0], location[1], turn)
+
+    if turn == 'b':
+        turn_rev = 'w'
+    else:
+        turn_rev = 'b'
+
+    for i in range(len(everywhere)):
+        direc = everywhere[i]
+        locate_now_posi = [location[0] + direc[0], location[1] + direc[1]]
+        reversi_locate = []
+
+        while 1:
+            try:
+                now_color = locate_list[locate_now_posi[0]][locate_now_posi[1]]
+            except:
+                reversi_locate = []
+                break
+            if locate_now_posi[0] < 0 or locate_now_posi[1] < 0:
+                reversi_locate = []
+                break
+
+            if now_color == turn_rev:
+                reversi_locate.append(locate_now_posi)
+            elif now_color == turn:
+                break
+            else:
+                reversi_locate = []
+                break
+            locate_now_posi = [locate_now_posi[0] + direc[0], locate_now_posi[1] + direc[1]]
+
+        for j in range(len(reversi_locate)):
+            x, y = reversi_locate[j]
+            put_koma(locate_list, x, y, turn)
+
+
+    return locate_list
+
+def change_turn(turn):
+    if turn == 'b':
+        return 'w'
+    else:
+        return 'b'
+
+def get_hist(history, location, turn):
+    data = '{}_{}'.format(location, turn)
+    history.append(data)
+    return history
+
+def undo_data(locate_list, history):
+    history.pop(-1)
+    locate_list_ver2 = get_initial_place()
+    for i in range(len(history)):
+        location, turn = history[i].rsplit('_', 1)
+        locate_list_ver2 = change_bord(locate_list_ver2, location, turn)
+    return locate_list_ver2, history
+
+def check(locate_list, location, turn):
+    everywhere = [[1, 1], [1, 0], [0, 1], [-1, 1], [-1, 0], [1, -1], [0, -1], [-1, -1]]
+    location = list(map(int, location.split('_')))
+    check_flg = 0
+
+    if turn == 'b':
+        turn_rev = 'w'
+    else:
+        turn_rev = 'b'
+
+    for i in range(len(everywhere)):
+        direc = everywhere[i]
+        locate_now_posi = [location[0] + direc[0], location[1] + direc[1]]
+        reversi_locate = []
+
+        while 1:
+            try:
+                now_color = locate_list[locate_now_posi[0]][locate_now_posi[1]]
+            except:
+                reversi_locate = []
+                break
+            if locate_now_posi[0] < 0 or locate_now_posi[1] < 0:
+                reversi_locate = []
+                break
+
+            if now_color == turn_rev:
+                reversi_locate.append(locate_now_posi)
+            elif now_color == turn:
+                break
+            else:
+                reversi_locate = []
+                break
+            locate_now_posi = [locate_now_posi[0] + direc[0], locate_now_posi[1] + direc[1]]
+
+        if len(reversi_locate) != 0:
+            check_flg = 1
+            break
+
+    return check_flg
+
+def check_next(locate_list, turn):
+    next_flg = 0
+    for i in range(8):
+        for j in range(8):
+            if locate_list[i][j] != '-':
+                continue
+            location = '{}_{}'.format(i, j)
+            if check(locate_list, location, turn):
+                next_flg = 1
+                break
+        if next_flg:
+            break
+
+    return next_flg
+
+
+
+def print_data(locate_list):
+    for i in range(len(locate_list)):
+        print(locate_list[i])
+
+if __name__ == '__main__':
+    locate_list = get_initial_place()
+    position_data = get_position()
+    width = [_ for _ in range(8)]
+    turn = 'b'
+    print_data(locate_list)
+    print()
+
+    location = '5_3'
+    locate_list = change_bord(locate_list, location, turn)
+    turn = change_turn(turn)
+    print_data(locate_list)
+    print()
+
+    location = '5_4'
+    locate_list = change_bord(locate_list, location, turn)
+    turn = change_turn(turn)
+    print_data(locate_list)
--- server_start.py
+++ server_start.py
@@ -10,22 +10,70 @@ app = Flask(__name__)
 # 初回はこちらにアクセス
 @app.route('/')
 def index():
-    return render_template('index.html', line=locate_list, title='HOHOHO')
+    return render_template('index.html', line=locate_list, title=title,
+                           width = width, position_data=position_data, turn=turn, judge=judge)
+
 
 # /post にアクセスしたときの処理（2回目以降はこちら）
 @app.route('/post', methods=['GET', 'POST'])
 def post():
-    global locate_list
-    if request.method == 'POST':
-        point_inf = request.form['point_inf']
-        locate_list = reversi.put_koma(locate_list, point_inf)
-        return render_template('index.html', line= locate_list, title='hogehoge')
+    global locate_list, turn, history
+
+    location = request.args.get('get_value')
+    if location == 'already':
+        judge = 'ng'
+    elif reversi.check(locate_list, location, turn):
+        locate_list = reversi.change_bord(locate_list, location, turn)
+        history = reversi.get_hist(history, location, turn)
+        turn = reversi.change_turn(turn)
+        judge = 'ok'
     else:
-        return redirect(url_for('index'))
+        judge = 'ng'
+
+    if reversi.check_next(locate_list, turn) == 0:
+        turn = reversi.change_turn(turn)
+        if reversi.check_next(locate_list, turn) == 0:
+            judge = 'finish'
+
+
+    return render_template('index.html', line=locate_list, title=title,
+                           width=width, position_data=position_data, turn=turn, judge=judge)
+
+
+@app.route('/undo', methods=['GET', 'POST'])
+def undo():
+    global locate_list, turn, history
+
+    locate_list, history = reversi.undo_data(locate_list, history)
+    turn = reversi.change_turn(turn)
+    judge = 'start'
+
+    return render_template('index.html', line= locate_list, title=title,
+                           width = width, position_data=position_data, turn=turn, judge=judge)
+
+
+@app.route('/restart', methods=['GET', 'POST'])
+def restart():
+    global locate_list, turn, history
+    locate_list = reversi.get_initial_place()
+    history = []
+    turn = 'b'
+    judge = 'start'
+    return render_template('index.html', line=locate_list, title=title,
+                           width=width, position_data=position_data, turn=turn, judge=judge)
+
 
 if __name__ == '__main__':
     # コマ情報を保持
     locate_list = reversi.get_initial_place()
+    position_data = reversi.get_position()
+    width = [_ for _ in range(8)]
+    history = []
+    turn = 'b'
+    judge = 'start'
+
+
+    title = 'REVERSI'
 
     app.debug = True
     app.run(host='0.0.0.0', port=8000)
